e9430ea7264a38209bed114e72119bf88c209091
Amit Ofeck <Amitofeck1@gmail.com>
2024-11-12 15:57:01 +0200

    install mutler

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -4,7 +4,6 @@ import verifyToken from '../middleware/verifyToken';
 
 const router = express.Router();
 
-// נתיב להרשמה
 router.post('/register', async (req, res) => {
     try {
       await registerUser(req, res);
@@ -14,18 +13,14 @@ router.post('/register', async (req, res) => {
   });
 
 
-// נתיב להתחברות
 router.post('/login', async (req, res) => {
   try {
-      await loginUser(req, res); // קורא לפונקציה loginUser, אשר מטפלת בתגובה בעצמה
+      await loginUser(req, res); 
   } catch (error) {
       res.status(400).json({ error: 'Invalid credentials' });
   }
 });
 
 
-// אם יש צורך בנתיב שמצריך אימות, נוכל להוסיף את המידלוואר verifyToken
-// לדוגמה:
-// router.get('/profile', verifyToken, getUserProfile);
 
 export default router;
