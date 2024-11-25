ee72e8a8be4d97b679aa750fe3aaf19f9652f19d
Amit Ofeck <Amitofeck1@gmail.com>
2024-11-25 17:15:17 +0200

    add router to main app to use navbar only for locations

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes , useLocation } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
-import CreatePost from './components/CreatePost';
-import Posts from './components/Posts';
 import NavBar from './components/NavBar';
 import Feed from './components/Feed';
 import PersonalPortal from './components/PersonalPortal';
@@ -14,16 +12,25 @@ import AllComments from './components/AllComments';
 function App() {
   return (
     <Router>
+      <Main />
+    </Router>
+  );
+}
+
+function Main() {
+  const location = useLocation(); 
+
+  return (
     <div className="App">
-      <NavBar />
+      {location.pathname !== '/login' && location.pathname !== '/register' && <NavBar />}
       <Routes>
         <Route path="/feed" element={<Feed />} /> 
         <Route path="/profile/:id" element={<PersonalPortal />} />
         <Route path="/comments/:postId" element={<AllComments />} />
-        {/* <Route path="/login" element={<Login />} />  */}
+        {/* <Route path="/login" element={<Login />} /> 
+        <Route path="/register" element={<Register />} /> */}
       </Routes>
     </div>
-  </Router>
   );
 }
 
