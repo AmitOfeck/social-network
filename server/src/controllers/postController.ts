58151a4a943dd87c13856857b3c10025476c7a22
Amit Ofeck <Amitofeck1@gmail.com>
2024-11-25 16:48:50 +0200

    renturs empty array if you dont have posts in getPostsByAuthorId

--- server/src/controllers/postController.ts
+++ server/src/controllers/postController.ts
@@ -47,12 +47,9 @@ export const createPostController = async (req: Request, res: Response): Promise
 export const getPostsByAuthorIdController = (req: Request, res: Response): void => {
   const { authorId } = req.params;  
 
-  getPostsByAuthorIdService(authorId) 
+  getPostsByAuthorIdService(authorId)  
       .then((posts) => {
-          if (!posts || posts.length === 0) {
-              return res.status(404).json({ message: 'No posts found for this author' });
-          }
-          return res.status(200).json(posts); 
+          return res.status(200).json(posts);  
       })
       .catch(() => {
           return res.status(500).json({ message: 'Failed to fetch posts' });
